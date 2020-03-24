3d5fe5e95cafcb6dc54c8a34d556b4bcfee49389
Rubens Back <rubensback@gmail.com>
2020-03-24 01:42:43 +0000

    Implementado useFetch para buscar dados do backEnd e container refactor

--- src/App.js
+++ src/App.js
@@ -1,20 +1,62 @@
 import React, {useState} from "react";
+import useFetch from 'use-http'
 import "./App.css";
+const date = new Date().toLocaleDateString()
+const AppUi = ({ handleSubmit, onInputChange, data, onDelete, loading, text }) => {
+  return (
+    <div className="global">
+      <h1> Bem-Vindo!</h1>
+      <h3> {date}</h3>
+      <div className="padded">
+        {/*Dar algum espaço entre o conteudo central e as margens do ecrã, padding prop do css FEITO*/}
+        <h2> NOTAS </h2>
+        <form className="marginRight" onSubmit={handleSubmit}>
+          {/*Dar uma margem entre a caixa de input e o butão, margin-right prop do css*/}
+          {/*Aumentar o tamanho da caixa de texto na qual o user escreve a nota
+          OU tornar a caixa responsiva de modo a adaptar-se ao texto que esta nela*/}
+          <input className="css-input" value={text} onChange={onInputChange} />
+          <button className="myButton">ADD</button>
+        </form>
+        <div>
+          {(loading && 'A carregar') || (data && data.map((n, index) => {
+            let note;
+            let key;
+            if (typeof n === 'string') {
+              note = n
+              key= note+index
+            } else {
+              note = n.note
+              key=n.id
+            }
+            return (
+              <li key={key}>
+                {/*Mudar a cor do texto para branco de modo a fazer contraste*/}
+                <span>{note}</span>
+                {/*Dar uma margem entre a nota e o butão, margin-right prop do css*/}
+                <button  className="buttonDelete" onClick={onDelete(index)}>Delete</button>
+              </li>
+            );
+          }))}
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default
-function App(props){
+function AppContainer(){
   const [text, setText ] = useState('');
-  const [list, setList ] = useState([
-    'Lorem Ipson cenas la da feira do  nao sei onde',
-    'Lorem Ipson marrocos africa egypto piramides e outras cenas boé da misticas',
-    'Lorem Ipson ela disse que um passarinho contou-lhe q ando a fumar. perguntei de voltei afinal quem fumar e fala com passaros ?',
-  ]);
+  const [list, setList ] = useState([]);
+  // const [shouldQuery, setShouldQuery] = useState(false)
 
-  function handleSubmit(e) {
+  const { loading, error, data } = useFetch('http://46.101.158.75:8091/notes', {}, [])
+  if(data && data !== list) {
+    setList(data)
+  }
+  async function handleSubmit(e) {
     e.preventDefault();
-    var prevList = list;
-    prevList.push(text);
-    setList(prevList);
+    // setShouldQuery(true);
+    alert('Funcionalidade de adicionar notas ainda não foi implementada')
     setText('');
   }
 
@@ -23,39 +65,12 @@ function App(props){
   }
 
   function removeItem(index) {
-    const prevList = list;
-    prevList.splice(index, 1);
-    setList(list)
+    // setList(list.filter((_,i) => i !== index ));
+    alert('Funcionalidade de delete ainda não foi implementada')
   }
-  
-  return (
-    <div className="global">
-      <h1> Bem-Vindo!</h1>
-      <h3> {new Date().toLocaleDateString()}</h3>
-      <div className="padded">
-        {/*Dar algum espaço entre o conteudo central e as margens do ecrã, padding prop do css FEITO*/}
-        <h2> NOTAS </h2>
-        <form className="marginRight" onSubmit={handleSubmit}>
-          {/*Dar uma margem entre a caixa de input e o butão, margin-right prop do css*/}
-          {/*Aumentar o tamanho da caixa de texto na qual o user escreve a nota
-          OU tornar a caixa responsiva de modo a adaptar-se ao texto que esta nela*/}
-          <input className="css-input" value={text} onChange={e => handleChange(e)} />
-          <button className="myButton">ADD</button>
-        </form>
-        <div>
-          {list.map((note, index) => {
-            return (
-              <li key={note+index}>
-                {/*Mudar a cor do texto para branco de modo a fazer contraste*/}
-                <span>{note}</span>
-                {/*Dar uma margem entre a nota e o butão, margin-right prop do css*/}
-                <button  className="buttonDelete" onClick={() => removeItem(index)}>Delete</button>
-              </li>
-            );
-          })}
-        </div>
-      </div>
-    </div>
-  );
-}
 
+  const onInputChange = e => handleChange(e)
+  const onDelete = index => () => removeItem(index) // function closure
+  
+  return AppUi({ handleSubmit, onInputChange, data, onDelete, loading, text });
+}
